88f73f70ce404366d6a95fe1298498ec7c680351
xdavius <xdavius@github.com>
2025-01-26 00:39:54 +0100

    fix install.sh : add path check and reload path

--- install.sh
+++ install.sh
@@ -12,21 +12,34 @@ ${nc}"
 }
 
 install() {
+# Add ~/.local/bin to PATH permanently if it's not already there
+if ! grep -q "$HOME/.local/bin" <<< "$PATH"; then
+    if [ -n "$BASH_VERSION" ]; then
+        echo -e 'export PATH="$HOME/.local/bin:$PATH"' >> "${HOME}/.bashrc"
+    elif [ -n "$ZSH_VERSION" ]; then
+        echo -e 'export PATH="$HOME/.local/bin:$PATH"' >> "${HOME}/.zshrc"
+    fi
+fi
+
 echo -e "${violet}Installation du binaire${nc}"
 if [[ ! -d "${HOME}"/.local/bin ]]; then
     mkdir -p "${HOME}"/.local/bin
 fi
-cp autocord.sh "${HOME}"/.local/bin/autocord
+cp "autocord.sh" "${HOME}"/.local/bin/autocord
 chmod +x "${HOME}"/.local/bin/autocord
 
 echo -e "${violet}Installation du service de mise à jour${nc}"
 if [[ ! -d "${HOME}"/.config/autostart ]]; then
-    mkdir -p "${HOME}"/.config/autostart/
+    mkdir -p "${HOME}"/.config/autostart
 fi
-cp autocord-autostart.desktop "${HOME}"/.config/autostart/autocord-autostart.desktop
+cp "autocord-autostart.desktop" "${HOME}"/.config/autostart/autocord-autostart.desktop
 
 echo -e "${violet}Mise à jour du path${nc}"
-. "${HOME}"/.bashrc
+if [ -n "$BASH_VERSION" ]; then
+    . "${HOME}/.bashrc"
+elif [ -n "$ZSH_VERSION" ]; then
+    . "${HOME}/.zshrc"
+fi
 
 echo -e "${violet}Installation de Discord via Autocord${nc}"
 bash "$HOME"/.local/bin/autocord install
